9d3dbfdf150e6b8f70ce04a33796d14eb44bd74a
alone141 <68744286+alone141@users.noreply.github.com>
2025-04-02 20:19:59 +0300

    Unit test???

--- Utility/test/src/UtilityUnitTest.cpp
+++ Utility/test/src/UtilityUnitTest.cpp
@@ -1,9 +1,14 @@
 #include <gtest/gtest.h>
-
-TEST(UtilityTests, DivideFunction) {
-    EXPECT_EQ(1, 1);
+#include <Utility.h>
+#include <cmath>
+TEST(UtilityTests, FactorialFunction) {
+    constexpr float factorialResult = utility::factorial(5);
+    EXPECT_EQ(120, factorialResult);
 }
-
+TEST(UtilityTests, PowFunction) {
+    constexpr float powResult = utility::pow(2.556, 3);
+    EXPECT_FLOAT_EQ(std::pow(2.556, 3), powResult);
+}
 // Main function for running tests
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
